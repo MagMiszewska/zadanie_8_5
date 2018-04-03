f1b782c794e69970a6954a5a1c6a89f86d6f742c
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-04-03 17:09:52 +0200

    Add html and js file with basic content

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a = prompt('Enter value for a'),
+	h = prompt('Enter value for h'); 
+
+var triangleArea = a * h / 2;
+
+alert('Triangle Area is: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
